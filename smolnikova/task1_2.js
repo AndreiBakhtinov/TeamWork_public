981fc93bacc2bafb075331a6e1a0f1cc6bf9aa31
Natalia Smolnikova <ntnatali@gmail.com>
2024-06-20 11:51:03 +0200

    new update3

---
+++ smolnikova/task1_2.js
@@ -0,0 +1,13 @@
+
+db.videos.aggregate([// на входе коллeкция
+    {
+        $match: {
+            duration_secs: { $lt: 120 * 60 }// на выходе ролики до 2 часов
+        }
+    },
+    { $count: 'total_videos' }// общее количество роликов до 2 часов
+])
+  // task2
+db.videos.countDocuments(
+{duration_secs: { $lt: 120 * 60 }
+})
---
+++ smolnikova/task3.js
@@ -0,0 +1,16 @@
+db.users.aggregate([
+    {
+        $sort: {
+            balance: 1
+        }
+    },
+    {
+        $limit: 2
+    },
+    {
+        $sample: {
+            size: 2
+        }
+    }
+])
+
