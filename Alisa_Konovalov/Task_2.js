29758df83e4ac7800fd8aa6a2b0f6276ec319b4f
Alisakono <alisa.konovalov@web.de>
2024-06-20 11:44:28 +0200

    Task 2

---
+++ Alisa_Konovalov/Task_2.js
@@ -0,0 +1,16 @@
+db.users.aggregate([
+    {
+        $sort: {
+            balance: -1
+        }
+    },
+    {
+        $limit: 2
+    }, {
+        $project: {
+            fullname: 1,
+            balance: 1,
+            _id: 0
+        }
+    }
+])
